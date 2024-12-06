50ebad4d703af2e0ed4cbc6e660e0c5b84d50463
Moffein <22742925+Moffein@users.noreply.github.com>
2024-12-05 22:15:21 -0600

    Removed GetComponent calls

--- TitanKillTimer/Class1.cs
+++ TitanKillTimer/Class1.cs
@@ -1,6 +1,8 @@
 ﻿using BepInEx;
 using RoR2;
 using System;
+using System.Collections.Generic;
+using System.Diagnostics;
 using UnityEngine;
 using UnityEngine.Networking;
 
@@ -13,88 +15,37 @@ namespace R2API.Utils
 }
 namespace BossKillTimer
 {
-    [BepInPlugin("com.Moffein.BossKillTimer", "Boss Kill Timer", "1.0.4")]
+    [BepInPlugin("com.Moffein.BossKillTimer", "Boss Kill Timer", "2.0.0")]
     public class BossKillTimer : BaseUnityPlugin
     {
         public static bool teleOnly = false;
         public static float instakillThreshold = 0f;
+
+        private static Dictionary<CharacterBody, float> dict = new Dictionary<CharacterBody, float>();
+
         public void Awake()
         {
             teleOnly = Config.Bind("Settings", "Teleporter Bosses Only", false, "Only show kill times when bosses with red healthbars die.").Value;
             instakillThreshold = Config.Bind("Settings", "Instakill Threshold", 1f, "Display an instakill message when the kill time is <= to this value.").Value;
-            On.RoR2.HealthComponent.TakeDamage += (orig, self, di) =>
-            {
-                if (NetworkServer.active
-                && self.body.isChampion
-                && (!teleOnly  || self.body.isBoss))
-                {
-                    if (!self.gameObject.GetComponent<KillTimerComponent>())
-                    {
-                        KillTimerComponent kt = self.gameObject.AddComponent<KillTimerComponent>();
-                        kt.bodyString = RoR2.Util.GetBestBodyName(self.gameObject);
-                        kt.StartTimer();
-                    }
-                }
-
-                orig(self, di);
-            };
-
-            On.RoR2.GlobalEventManager.OnCharacterDeath += (orig, self, damageReport) =>
-            {
-                orig(self, damageReport);
-
-                if (NetworkServer.active)
-                {
-                    if (damageReport.victim && damageReport.victimIsChampion && (!teleOnly || damageReport.victimIsBoss))
-                    {
-                        KillTimerComponent kt = damageReport.victim.GetComponent<KillTimerComponent>();
-                        if (kt)
-                        {
-                            kt.EndTimer();
-                        }
-                    }
-                }
-            };
-        }
-    }
-
-    public class KillTimerComponent : MonoBehaviour
-    {
-        public void FixedUpdate()
-        {
-            if (timerStarted)
-            {
-                stopwatch += Time.fixedDeltaTime;
-            }
+            RoR2.Stage.onStageStartGlobal += Stage_onStageStartGlobal;
+            On.RoR2.HealthComponent.TakeDamage += HealthComponent_TakeDamage;
         }
 
-        public void StartTimer()
+        private void GlobalEventManager_OnCharacterDeath(On.RoR2.GlobalEventManager.orig_OnCharacterDeath orig, GlobalEventManager self, DamageReport damageReport)
         {
-            if (!timerStarted)
+            orig(self, damageReport);
+            if (!NetworkServer.active || !(damageReport.victimBody && dict.ContainsKey(damageReport.victimBody))) return;
+
+            if (dict.TryGetValue(damageReport.victimBody, out float startTime))
             {
-                timerStarted = true;
-                stopwatch = 0f;
-            }
-        }
-
-        private void OnDestroy()
-        {
-            EndTimer();
-        }
-
-        public void EndTimer()
-        {
-            if (!hasDied)
-            {
-                hasDied = true;
-
+                float stopwatch = Time.time - startTime;
                 bool instaKill = stopwatch <= BossKillTimer.instakillThreshold;
                 string deathString = string.Empty;
                 if (instaKill)
                 {
                     deathString += "<style=cIsHealing>INSTANT KILL!</style> ";
                 }
-                deathString += "<style=cIsHealth>" + bodyString + "</style>";
+                deathString += "<style=cIsHealth>" + Util.GetBestBodyName(damageReport.victimBody.gameObject) + "</style>";
                 deathString += " was killed in ";
                 deathString += "<style=cIsDamage>" + stopwatch + "</style>";
                 deathString += " seconds!";
@@ -103,12 +54,24 @@ namespace BossKillTimer
                 {
                     baseToken = deathString
                 });
+
+                dict.Remove(damageReport.victimBody);
             }
         }
 
-        float stopwatch;
-        public bool timerStarted = false;
-        public bool hasDied = false;
-        public string bodyString = "BOSS";
+        private void HealthComponent_TakeDamage(On.RoR2.HealthComponent.orig_TakeDamage orig, HealthComponent self, DamageInfo damageInfo)
+        {
+            orig(self, damageInfo);
+            if (!NetworkServer.active || !(self.body && self.body.isChampion && self.body.teamComponent && self.body.teamComponent.teamIndex != TeamIndex.Player)) return;
+            if (!dict.ContainsKey(self.body))
+            {
+                dict.Add(self.body, Time.time);
+            }
+        }
+
+        private void Stage_onStageStartGlobal(Stage obj)
+        {
+            dict.Clear();
+        }
     }
 }
